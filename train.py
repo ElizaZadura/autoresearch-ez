faf3dfccb777be0b0682eb7af16c3608e180c5a6
Eliza Zadura <eliza.zadura@gmail.com>
2026-04-02 01:54:41 +0200

    Revert "Increase Muon momentum target from 0.95 to 0.97."
    This reverts commit 8e0b5aa52bc6aba43d12d8288cae486ed7ab2b7a.

--- train.py
+++ train.py
@@ -746,7 +746,7 @@ def get_lr_multiplier(progress):
 
 def get_muon_momentum(step):
     frac = min(step / 100, 1)
-    return (1 - frac) * 0.85 + frac * 0.97
+    return (1 - frac) * 0.85 + frac * 0.95
 
 def get_weight_decay(progress):
     return WEIGHT_DECAY
